6faec7878297785223156812244f58161cb78b70
gbalderas <balderas@lenovo-laptop>
2015-10-27 10:40:28 +0100

    optimized timer

--- src/main/java/model/Timer.java
+++ src/main/java/model/Timer.java
@@ -7,6 +7,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.util.Duration;
 
+//TODO use Apache Commons time.Stopwatch instead of Timeline
 public class Timer {
 
 	private static Boolean started = false;
@@ -15,25 +16,25 @@ public class Timer {
 	public static StringProperty timeCounter = new SimpleStringProperty();
 
 	private static int minutes;
-	private static double seconds;
+	private static int seconds;
 	private static Duration time = Duration.ZERO;
 
 	public static void startTime() {
 
 		if (!started) {
 			started = true;
-			timeline = new Timeline(new KeyFrame(Duration.millis(100), t -> {
-				Duration duration = ((KeyFrame) t.getSource()).getTime();
-				time = time.add(duration);
-				seconds = time.toSeconds();
-				if (seconds >= 60) {
+			timeCounter.set("0:00");
+			timeline = new Timeline(new KeyFrame(Duration.seconds(1), t -> {
+				time = time.add(Duration.seconds(1));
+				if (seconds == 59) {
 					time = Duration.ZERO;
 					minutes++;
 				}
-				if (seconds < 10)
-					timeCounter.set(minutes + ":0" + seconds);
-				else
+				seconds = (int) time.toSeconds();
+				if (seconds > 9)
 					timeCounter.set(minutes + ":" + seconds);
+				else
+					timeCounter.set(minutes + ":0" + seconds);
 			}));
 			timeline.setCycleCount(Animation.INDEFINITE);
 			timeline.play();
@@ -54,7 +55,6 @@ public class Timer {
 				GameInfo.enableGrid();
 				timeline.play();
 			}
-
 	}
 
 	public static void resetTime() {
